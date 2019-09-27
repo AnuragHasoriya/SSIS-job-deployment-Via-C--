d53f0f5a8a8ae6bb5fee25172a9ad37b9273fff7
Anurag <anuhasoriya@gmail.com>
2019-09-27 19:15:27 +0530

    deployment added

---
+++ ERP.Jobs.Deployment/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.SqlServer.Management.IntegrationServices;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Jobs.Deployment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string targetFolderName = "dailyClosedJobs";
+            string projectName = "dailyClosedJobs.dtsx";
+            string projectFilePath = @"" + ConfigurationManager.AppSettings["FolderPath"];
+
+            try
+            {
+                // Create a connection to the server// Create the Integration Services object
+                string conStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                SqlConnection sqlConnection = new SqlConnection(conStr);
+                using (var con = sqlConnection)
+                {
+                    IntegrationServices integrationServices = new IntegrationServices(con);
+
+                    // Get the Integration Services catalog
+                    Catalog catalog = integrationServices.Catalogs["SSISDB"];
+
+                    // Create the target folder
+                    CatalogFolder folder = new CatalogFolder(catalog,
+                        targetFolderName, "Folder description");
+
+                    Console.WriteLine("Deploying " + projectName + " project.");
+
+                    byte[] projectFile = File.ReadAllBytes(projectFilePath);
+                    folder.DeployProject(projectName, projectFile);
+
+                    Console.WriteLine("Done.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ ERP.Jobs.Deployment/SqlHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Jobs.Deployment
+{
+    public class SqlHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            if (connectionDBName == null)
+                return new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            else
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                string user = builder.UserID;
+                string pass = builder.Password;
+                string credentials = user != "" && pass != "" ? (";User=" + user + "; Password = " + pass) : ";Integrated Security=true;";
+                return new SqlConnection("Data Source=" + builder.DataSource + ";MultipleActiveResultSets=True;Initial Catalog=" + connectionDBName + credentials);
+            }
+        }
+        public static object connectionDBName { get; set; }
+    }
+}
